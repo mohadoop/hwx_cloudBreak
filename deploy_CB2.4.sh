bd098178b95cba213981ce7d81e49e329d6cefb3
mohiuddinopu <mohiuddin@Mohammeds-MacBook-Pro.local>
2018-03-07 15:08:24 -0500

    little formatting

--- deploy_CB2.4.sh
+++ deploy_CB2.4.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-yum update -y
-yum -y install iptables-services net-tools
-
-iptables --flush INPUT && \
-iptables --flush FORWARD && \
-service iptables save
-
-
+# Add docker repo
 sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
 name=Docker Repository
@@ -17,17 +10,30 @@ gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
-yum -y install docker-engine
+# Update yum and install required tools
+yum update -y
+yum -y install iptables-services net-tools docker-engine unzip tar
+
+
+# OS configuraiton for Docker and CB
 systemctl enable docker.service
 systemctl start docker
+iptables --flush INPUT && \
+iptables --flush FORWARD && \
+service iptables save
 
-yum -y install unzip tar
+# Download and place CloudBreak into /bin directory
 curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_2.4.0_Linux_x86_64.tgz | sudo tar -xz -C /bin cbd
 cbd --version
 
+# Create CB deployment directory
 rm -rf ~/cloudbreak-deployment
 mkdir ~/cloudbreak-deployment
 
+
+
+#### Change the CB UI login user name and password here
+# Create Profile file with initial configurations
 tee ~/cloudbreak-deployment/Profile <<-'EOF'
 export UAA_DEFAULT_SECRET=MySecret123
 export UAA_DEFAULT_USER_PW=MySecurePassword123
@@ -37,6 +43,7 @@ EOF
 
 
 
+# Generate CB deployment and start 
 rm ~/cloudbreak-deployment/*.yml
 cd ~/cloudbreak-deployment
 cbd generate
